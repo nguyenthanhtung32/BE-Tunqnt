cf98b20ee8181638bf1d9c5a1c32138477e38a63
nguyenthanhtung32 <binbungbu6201@gmail.com>
2023-04-19 23:13:09 +0700

    first commit

---
+++ constants/dbSettings.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CONNECTION_STRING: 'mongodb://127.0.0.1:27017/training-database',
+  DATABASE_NAME: 'training-database',
+
+  // CONNECTION_STRING: 'mongodb+srv://new-user-1:123123123@cluster0.mrci4cv.mongodb.net/test',
+  // DATABASE_NAME: 'training-database',
+};
